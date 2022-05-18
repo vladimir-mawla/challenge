1307594d63f6253a3b4a60e3bec4a8a5d0a14b75
vladimir <vladimirmawla2@gmail.com>
2022-05-18 19:26:22 +0300

    Adding sound buttons on js file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,28 +5,41 @@ window.addEventListener('keydown', (event) => {
   }
 });
 
+/*Audio*/
+
+var green_audio = new Audio("./sounds/green.mp3");
+var red_audio = new Audio("./sounds/red.mp3");
+var yellow_audio = new Audio("./sounds/yellow.mp3");
+var blue_audio = new Audio("./sounds/blue.mp3");
+var wrong_audio = new Audio("./sounds/wrong.mp3")
+
 const green = document.getElementById('green');
 
 green.addEventListener('click', function handleClick() {
   console.log('green clicked');
+  green_audio.play();
+
 });
 
 const red = document.getElementById('red');
 
 red.addEventListener('click', function handleClick() {
   console.log('red clicked');
+  red_audio.play();
 });
 
 const yellow = document.getElementById('yellow');
 
 yellow.addEventListener('click', function handleClick() {
   console.log('yellow clicked');
+  yellow_audio.play();
 });
 
 const blue = document.getElementById('blue');
 
 blue.addEventListener('click', function handleClick() {
   console.log('blue clicked');
+  blue_audio.play();
 });
 
 
