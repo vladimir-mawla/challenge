1fff7d2b1d5007b2ab508b9a58c33c44394f48ab
vladimir <vladimirmawla2@gmail.com>
2022-05-18 04:36:55 +0300

    Tryinh javascript

---
+++ scripts/scripts.js
@@ -0,0 +1,13 @@
+const btn = document.getElementsByTagName('button');
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'red';
+  btn.style.color = 'white';
+});
+/*
+function playSound() {
+    var sound = document.getElementById("audio");
+    sound.play();
+}
+?
+*/
