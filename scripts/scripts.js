89dcc3b2c0945dcd83919b43cc3a2f696be74d95
vladimir <vladimirmawla2@gmail.com>
2022-05-18 13:23:52 +0300

    Getting the button pressed js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,32 @@
-const btn = document.getElementsByTagName('button');
+window.addEventListener('keydown', (event) => {
+  if(event) {
+    event.preventDefault();
+    document.getElementById("text").innerHTML = "LEVEL 1"
+  }
+});
 
-btn.addEventListener('click', function onClick() {
-  btn.style.backgroundColor = 'red';
-  btn.style.color = 'white';
+const green = document.getElementById('green');
+
+green.addEventListener('click', function handleClick() {
+  console.log('green clicked');
+});
+
+const red = document.getElementById('red');
+
+red.addEventListener('click', function handleClick() {
+  console.log('red clicked');
+});
+
+const yellow = document.getElementById('yellow');
+
+yellow.addEventListener('click', function handleClick() {
+  console.log('yellow clicked');
+});
+
+const blue = document.getElementById('blue');
+
+blue.addEventListener('click', function handleClick() {
+  console.log('blue clicked');
 });
 /*
 function playSound() {
